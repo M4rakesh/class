204edaf76ac44c1e24b43e148d399696212f5772
M4rakesh <maksims.rogozins@gmail.com>
2024-09-17 11:32:13 +0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,16 +1,34 @@
 class Veicle:
+    total=0
     def __init__(self,make,model,year,price):
         self.make=make
         self.model=model
         self.year=year
         self.price=price
-
+    def total_vehicles(self):
+        Veicle.total+=self.price
+        print(f"Auto cena ir {Veicle.total}")
+        return Veicle.total
     def display_info(self):
         print(f"Marka;{self.make},Model:{self.model},Year:{self.year},Price:{self.price}")
-class Car:
+class Car(Veicle):
     def __init__(self,make,model,year,price,num_doors,body_style):
         super().__init__(make,model,year,price)
         self.num_doors=num_doors
         self.body_style=body_style
-a=Veicle("Audi","a6",2006,3400,5,"universals")
+    def display_info(self):
+        print(f"Marka;{self.make},Model:{self.model},Year:{self.year},Price:{self.price},Doors:{self.num_doors},Body Type:{self.body_style}")
+class Truck(Veicle):
+    def __init__(self,make,model,year,price,bed_leght,towing_capacity):
+        super().__init__(make,model,year,price)
+        self.bed_leght=bed_leght
+        self.towing_capacity=towing_capacity
+    def display_info(self):
+        print(f"Marka;{self.make},Model:{self.model},Year:{self.year},Price:{self.price},Bed leght:{self.bed_leght},towing capacity:{self.towing_capacity}")
+
+a=Car("Audi","a6",2006,3400,5,"universals")
 a.display_info()
+a.total_vehicles()
+truck=Truck("Ford","FM106",2023,30000,"6162","13 t")
+truck.display_info()
+truck.total_vehicles()
