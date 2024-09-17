0a9b6d159e55778a70d91a7c1fe292ba16200809
M4rakesh <maksims.rogozins@gmail.com>
2024-09-17 11:15:45 +0300

    Update auto.py
    1

--- auto.py
+++ auto.py
@@ -7,6 +7,10 @@ class Veicle:
 
     def display_info(self):
         print(f"Marka;{self.make},Model:{self.model},Year:{self.year},Price:{self.price}")
-
-a=Veicle("Audi","a6",2006,3400)
+class Car:
+    def __init__(self,make,model,year,price,num_doors,body_style):
+        super().__init__(make,model,year,price)
+        self.num_doors=num_doors
+        self.body_style=body_style
+a=Veicle("Audi","a6",2006,3400,5,"universals")
 a.display_info()
