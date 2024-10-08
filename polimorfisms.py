0b0ee2a15260acf70d77e21ef07c9a45da0590df
M4rakesh <maksims.rogozins@gmail.com>
2024-10-08 10:46:52 +0300

    1
    1

--- polimorfisms.py
+++ polimorfisms.py
@@ -57,7 +57,7 @@ for i in farm_band:
     if isinstance(i,Cow):
         i.cow_voice()'''
 
-class Animal:
+'''class Animal:
     legs=4
     tail=1
 
@@ -88,4 +88,36 @@ cow1,cow2=Cow(),Cow()
 
 farm_band=[cat1,cat2,dog1,dog2,cow1,cow2]
 for i in farm_band:
-    i.voice()
+    i.voice()'''
+import math
+class Shape:
+    def area(self):
+        pass
+    def perimetr(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+    def area(self):
+        return self.width * self.height
+    def perimetr(self):
+        return 2 * (self.width + self.height)
+    
+class Circle(Shape):
+
+    def __init__(self,radiuss):
+        self.radiuss=radiuss
+    def perimetr(self):
+        return 2*math.pi * self.radiuss
+    def area(self):
+        return (math.pi * self.radiuss**2)
+    
+c1=Circle(1.5)
+print(c1.area())
+print(c1.perimetr())
+
+shapes=[Rectangle(3,4),Circle(5)]
+for shape in shapes:
+    print(f"Area:{shape.area():.2f},Perimetr:{shape.perimetr():.2f}")
